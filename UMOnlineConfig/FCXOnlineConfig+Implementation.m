266a2d62e567e42a039f6e9a23390b6029e36daf
fengchuanxiang <fengchuanxiang@126.com>
2016-04-09 09:26:47 +0800

    修改FCXonlineConfigure

--- UMOnlineConfig/FCXOnlineConfig+Implementation.m
+++ UMOnlineConfig/FCXOnlineConfig+Implementation.m
@@ -8,10 +8,20 @@
 
 #import "FCXOnlineConfig+Implementation.h"
 #import "UMOnlineConfig.h"
+#import <objc/runtime.h>
+
 
 @implementation FCXOnlineConfig (Implementation)
 
-+ (NSString *)fcxGetConfigParams:(NSString *)key defaultValue:(NSString *)defaultValue {
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        SwizzleClassMethod([self class], @selector(fcxGetConfigParams:defaultValue:), @selector(fcxImplementationGetConfigParams:defaultValue:));
+    });
+}
+
++ (NSString *)fcxImplementationGetConfigParams:(NSString *)key defaultValue:(NSString *)defaultValue {
     NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
     NSString *versionParam = [NSString stringWithFormat:@"%@_%@", key, appVersion];
     
@@ -24,8 +34,20 @@
             result = defaultValue;
         }
     }
-    
     return result;
+}
+
+void SwizzleClassMethod(Class c, SEL orig, SEL new) {
+    
+    Method origMethod = class_getClassMethod(c, orig);
+    Method newMethod = class_getClassMethod(c, new);
+    
+    c = object_getClass((id)c);
+    
+    if(class_addMethod(c, orig, method_getImplementation(newMethod), method_getTypeEncoding(newMethod)))
+        class_replaceMethod(c, new, method_getImplementation(origMethod), method_getTypeEncoding(origMethod));
+    else
+        method_exchangeImplementations(origMethod, newMethod);
 }
 
 @end
