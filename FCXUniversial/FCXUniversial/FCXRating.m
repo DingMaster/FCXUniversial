70e86f4351bcbebd51d0f76314526d879e84d9d3
fengchuanxiang <fengchuanxiang@126.com>
2016-04-09 09:24:58 +0800

    修改好评逻辑

--- FCXUniversial/FCXUniversial/FCXRating.m
+++ FCXUniversial/FCXUniversial/FCXRating.m
@@ -1,5 +1,5 @@
 //
-//  FCXRating.h
+//  FCXRating.m
 //  Universial
 //
 //  Created by 冯 传祥 on 15/8/23.
@@ -9,18 +9,19 @@
 #import "FCXRating.h"
 #import "FCXGuide.h"
 #import "UMFeedback.h"
-#import "FCXOnlineConfig.h"
-#import "MobClick.h"
+
 
 #define HASRATING @"HasRating"
 
 @implementation FCXRating
 
-+ (void)startRating:(NSString *)appID {
-    [[FCXRating sharedRating] fcx_startRating:appID];
++ (void)startRating {
+    
+    [[FCXRating sharedRating] fcx_startRating];
 }
 
 + (FCXRating *)sharedRating {
+    
     static FCXRating *rating;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
@@ -29,9 +30,9 @@
     return rating;
 }
 
-- (void)fcx_startRating:(NSString *)appID {
-  
-    BOOL showRating = [[FCXOnlineConfig fcxGetConfigParams:@"showRating" defaultValue:@"0"] boolValue];
+- (void)fcx_startRating {
+    
+    BOOL showRating = [[UMOnlineConfig fcxGetConfigParams:@"showRating" defaultValue:@"0"] boolValue];
     if (!showRating) {
         return;
     }
@@ -40,53 +41,99 @@
     if (self.hasRating) {
         return;
     }
-    static int i = 0;
-    if (i > 0) {
-        return;
-    }
-//    NSString *paramsString = @"{\"标题\" : \"铃声\", \"内容\" : \"导流测试\", \"按钮1\" : \"赏个好评\", \"按钮2\" : \"我要吐槽\", \"按钮3\" : \"残忍的拒绝\"}";
-    NSString *paramsString = [FCXOnlineConfig fcxGetConfigParams:@"ratingContent" defaultValue:@""];
+    
+    //    NSString *paramsString = @"{\"标题\" : \"铃声\", \"内容\" : \"导流测试\", \"按钮1\" : \"赏个好评\", \"按钮2\" : \"我要吐槽\", \"按钮3\" : \"残忍的拒绝\"}";
+    NSString *paramsString = [UMOnlineConfig fcxGetConfigParams:@"ratingContent" defaultValue:@""];
     NSDictionary *paramsDict = [NSJSONSerialization JSONObjectWithData:[paramsString dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableLeaves error:nil];
-//    NSLog(@"==%@", paramsDict);
+    //    NSLog(@"==%@", paramsDict);
     NSString *title = [paramsDict objectForKey:@"标题"];
     NSString *content = [paramsDict objectForKey:@"内容"];
     NSString *btn1 = [paramsDict objectForKey:@"按钮1"];
     NSString *btn2 = [paramsDict objectForKey:@"按钮2"];
-    NSString *btn3 = [paramsDict objectForKey:@"按钮3"];
-
-    if (!title || !content || !btn1 || !btn2 || !btn3) {
+    //    NSString *btn3 = [paramsDict objectForKey:@"按钮3"];
+    NSInteger alertTimes = [[paramsDict objectForKey:@"总提醒次数"] integerValue];
+    
+    if (!title || !content || !btn1 || !btn2) {
         return;
     }
     
-    i++;
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSString *currentDateString = [self getCurrentDateString];
+    NSString *alertDateString = [userDefaults objectForKey:@"alertDate"];
+    if (alertDateString && [alertDateString isEqualToString:currentDateString]) {//当天弹出过
+        return;
+    }
     
-    MAlertViw *alertView = [[MAlertViw alloc] initWithTitle:title message:content delegate:nil cancelButtonTitle:nil otherButtonTitles:btn1, btn2, btn3, nil];
-            alertView.dismiss = YES;
+    if ([userDefaults integerForKey:@"alertTimes"] >= alertTimes) {//超过弹出次数
+        return;
+    }
+    
+    MAlertViw *alertView = [[MAlertViw alloc] initWithTitle:title message:content delegate:nil cancelButtonTitle:nil otherButtonTitles:btn1, btn2, nil];
+    alertView.dismiss = YES;
     [alertView show];
     
     alertView.handleAction = ^(MAlertViw *alertView, NSInteger buttonIndex){
-        [FCXRating saveRating];
-
+        //        [FCXRating saveRating];
+        
         if (buttonIndex == 0) {
-            [FCXRating goRating:appID];
-        }else if(buttonIndex == 1) {
             
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 UIViewController *vc = [UIApplication sharedApplication].keyWindow.rootViewController;
                 [vc presentViewController:[UMFeedback feedbackModalViewController] animated:YES completion:^{
-
+                    
                 }];
             });
-          
+            
+        }else if(buttonIndex == 1) {
+            
+            NSString *strUrl =[NSString stringWithFormat: @"itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@", APPID];
+            if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:strUrl]]) {
+                [[UIApplication sharedApplication] openURL:[NSURL URLWithString:strUrl]];
+            }else{
+                strUrl =[NSString stringWithFormat: @"https://itunes.apple.com/app/id%@", APPID];
+                [[UIApplication sharedApplication] openURL:[NSURL URLWithString:strUrl]];
+            }
+            
+            [FCXRating saveRating];
+            
         }else {
-
+            //            [FCXRating saveRating];
         }
-      };
+    };
+    
+    [self saveAlert];
+}
 
+//保存提醒的日期和次数
+- (void)saveAlert {
+    
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSString *currentDateString = [self getCurrentDateString];
+    NSInteger alertTimes = [userDefaults integerForKey:@"alertTimes"];
+    alertTimes++;
+    
+    [userDefaults setObject:currentDateString forKey:@"alertDate"];
+    [userDefaults setObject:[NSNumber numberWithInteger:alertTimes] forKey:@"alertTimes"];
+    [userDefaults synchronize];
+}
+
+//获取当前时间的字符串
+- (NSString *)getCurrentDateString {
+    
+    NSMutableDictionary *threadDictionary = [[NSThread currentThread] threadDictionary] ;
+    NSDateFormatter *dateFormatter = [threadDictionary objectForKey: @"RatingDateFormatter"] ;
+    if (dateFormatter == nil)
+    {
+        dateFormatter = [[NSDateFormatter alloc] init] ;
+        [dateFormatter setDateFormat: @"YYYY-MM-dd"] ;
+        [threadDictionary setObject: dateFormatter forKey: @"RatingDateFormatter"] ;
+    }
+    return [dateFormatter stringFromDate:[NSDate date]];
 }
 
 //检查版本，如果版本不一致，清除之前版本的缓存
 - (void)checkAppVersion {
+    
     NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
     NSString *ratingVersion = [userDefaults objectForKey:@"RatingAppVersion"];
     NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
@@ -94,7 +141,7 @@
     if (!ratingVersion) {//之前没有存过版本，第一次存
         
         ratingVersion = appVersion;
-        [userDefaults setObject:ratingVersion forKey:@"RatingAppVersion"];        
+        [userDefaults setObject:ratingVersion forKey:@"RatingAppVersion"];
     }else if(![ratingVersion isEqualToString:appVersion]) {//版本升级，清空之前缓存
         
         ratingVersion = appVersion;
@@ -102,6 +149,7 @@
         
         //清楚之前版本的缓存
         [userDefaults removeObjectForKey:HASRATING];
+        [userDefaults removeObjectForKey:@"alertTimes"];
     }
     [userDefaults synchronize];
 }
@@ -115,36 +163,23 @@
     [[NSUserDefaults standardUserDefaults]synchronize];
 }
 
-+ (void)goAppStore:(NSString*)appID {
-    if (![appID isKindOfClass:[NSString class]]) {
++ (void)gotoITunesApplicationPage:(NSString*)strAppID {
+    // 应用内打开
+    if (![strAppID isKindOfClass:[NSString class]] && strAppID.length > 0) {
         return;
     }
-    // 打开应用内购买
     SKStoreProductViewController *vc = [[SKStoreProductViewController alloc] init];
     
     vc.delegate = [FCXRating sharedRating];
-    NSDictionary *dict = [NSDictionary dictionaryWithObject:appID forKey:SKStoreProductParameterITunesItemIdentifier];
+    NSDictionary *dict = [NSDictionary dictionaryWithObject:strAppID forKey:SKStoreProductParameterITunesItemIdentifier];
     [vc loadProductWithParameters:dict completionBlock:nil];
     
     [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:vc animated:YES completion:nil];
-}
-     
-- (void)productViewControllerDidFinish:(SKStoreProductViewController *)viewController {
-    [viewController dismissViewControllerAnimated:YES completion:nil];
+    
 }
 
-+ (void)goRating:(NSString *)appID {
-    if (![appID isKindOfClass:[NSString class]]) {
-        return;
-    }
-    
-    NSString *strUrl =[NSString stringWithFormat: @"itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@", appID];
-    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:strUrl]]) {
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:strUrl]];
-    }else{
-        strUrl =[NSString stringWithFormat: @"https://itunes.apple.com/app/id%@", appID];
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:strUrl]];
-    }
+- (void)productViewControllerDidFinish:(SKStoreProductViewController *)viewController {
+    [viewController dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
